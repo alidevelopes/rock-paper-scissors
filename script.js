0563725b64ca77a3fadef26d13b695be3fe11527
alidevelopes <alidevelopes314@gmail.com>
2023-10-02 00:05:59 +0200

    Edit css, html and js file.
    CSS: add style to button and fonts for better visual experience.
    html: edit class attributes in "main" to make it easier to work with in css, and scripe file.
    js: rewrite almost all functions to improve UI.

--- script.js
+++ script.js
@@ -1,129 +1,103 @@
 // In this code, rock, paper and scissors are assigned to num 0, 1, 2, respectively.
 
-// making a random number generator for computer to imitate choice
-function computerNum() {
+// score
+let computerScore = 0;
+let playerScore = 0;
+let singleRoundWinner = "";
+
+// get computer choice
+function getComputerChoice() {
+  let compChoice = "";
   let randNum = Math.random();
   randNum = Math.floor(randNum * 3);
-  return randNum;
-}
+  switch (randNum) {
+    case 0:
+      compChoice = "rock";
+      break;
+    case 1:
+      compChoice = "paper";
+      break;
 
-// This function asks for player's input and assign it to num in order to compare it with computer's
-function playerSelection() {
-  let playerInput = prompt("Type you choice \nRock / Paper / Scissor?  ");
-  playerInput = playerInput.toLowerCase();
-
-  let playerNum = 0;
-  if (playerInput === "paper") {
-    playerNum = 1;
-  } else if (playerInput === "scissor") {
-    playerNum = 2;
+    case 2:
+      compChoice = "scissor";
+      break;
   }
-  return playerNum;
+  return compChoice;
 }
 
-// creating a new function to compare player and computer's choices,
-function playRound() {
-  let score = 0;
-  let compScore = 0;
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorBtn = document.querySelector("#scissorBtn");
+const scoreContainer = document.querySelector("#score-container");
 
-  // win/ lose + player/computer choice
-  let playerNum = playerSelection(),
-    computer = computerNum();
+rockBtn.addEventListener("click", clickHandler);
+paperBtn.addEventListener("click", clickHandler);
+scissorBtn.addEventListener("click", clickHandler);
 
-  let winMsg = `You win. You chose ${numToChoice(
-    playerNum
-  )}, computer chose ${numToChoice(computer)}.`;
+const playerScorePara = document.querySelector("#player-score");
+const computerScorePara = document.querySelector("#computer-score");
+const finalResultPara = document.querySelector(".result-para");
 
-  let loseMsg = `You lose. You chose ${numToChoice(
-    playerNum
-  )}, computer chose ${numToChoice(computer)}.`;
-
-  let drawMsg = `It's a draw. You chose ${numToChoice(
-    playerNum
-  )}, computer chose ${numToChoice(computer)}.`;
-
+//  get a single round winner
+function playRound(playerChoice, computerChoice) {
   // condition for draw
-  if (playerNum === computer) {
-    alert(drawMsg);
-  } else if (playerNum === 0) {
-    // conditions for player to win if player chooses "rock"
-    if (computer === 2 && computer != 1) {
-      alert(winMsg);
-      score += 1;
-    } else {
-      alert(loseMsg);
-      compScore += 1;
-    }
-  } else if (playerNum === 1) {
-    // conditions for player to win if player chooses "paper"
-    if (computer === 0 && computer != 2) {
-      alert(winMsg);
-      score += 1;
-    } else {
-      alert(loseMsg);
-      compScore += 1;
-    }
-
-    // if player chose "scissor":
-  } else if (playerNum === 2) {
-    // conditions for player to win if player chooses "paper"
-    if (computer === 1 && computer != 0) {
-      alert(winMsg);
-      score += 1;
-    } else {
-      alert(loseMsg);
-      compScore += 1;
-    }
+  if (playerChoice === computerChoice) {
+    // singleRoundWinner = "It's a draw";
   }
-
-  let roundResult = [score, compScore];
-  // return score;
-  // alert(roundResult);
-  return roundResult;
+  // player wins
+  if (
+    (playerChoice === "rock" && computerChoice === "scissor") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissor" && computerChoice === "paper")
+  ) {
+    playerScore++;
+    singleRoundWinner = "player";
+  }
+  // computer wins
+  if (
+    (playerChoice === "rock" && computerChoice === "paper") ||
+    (playerChoice === "paper" && computerChoice === "scissor") ||
+    (playerChoice === "scissor" && computerChoice === "rock")
+  ) {
+    computerScore++;
+    singleRoundWinner = "computer";
+  }
+  // adding score for display
+  playerScorePara.textContent = playerScore;
+  computerScorePara.textContent = computerScore;
 }
-// playRound();
 
-// convert 0, 1, 2 back to rock, paper, scissor, respectively.
-// the purpose of this function is to show the choices made in text rather than num.
-function numToChoice(n) {
-  let choice = "Rock";
-  if (n === 1) {
-    choice = "Paper";
-  } else if (n === 2) {
-    choice = "Scissor";
+function getGameStatus() {
+  let keepPlaying = true;
+  if (playerScore === 5) {
+    keepPlaying = false;
+  } else if (computerScore === 5) {
+    keepPlaying = false;
   }
-  return choice;
+
+  return keepPlaying;
 }
-// some
 
-// this function keeps track of number of rounds played and player's score
-function game(totalRounds) {
-  let result;
-  let playerScore = 0;
-  let compScore = 0;
-
-  i = 0;
-  while (i < totalRounds) {
-    result = playRound();
-    playerScore += result[0];
-    compScore += result[1];
-
-    i += 1;
+function endGameWinner() {
+  let WINNER = "";
+  if (playerScore === 5) {
+    WINNER = `Congrats! You are the winner.`;
+  } else if (computerScore === 5) {
+    WINNER = `You lose. Computer wins.`;
   }
-
-  let matchResult = "";
-  let drawMatches = totalRounds - (playerScore + compScore);
-  if (playerScore === compScore) {
-    matchResult = `It's a draw. You won ${playerScore} time(s) and computer won ${compScore} time(s).`;
-  } else if (playerScore > compScore) {
-    matchResult = `You win! You played ${totalRounds} rounds and won ${playerScore} time(s)!`;
-  } else {
-    matchResult = `Final result:\nYou played ${totalRounds} rounds, tied ${drawMatches} and lost ${compScore} time(s). You lose:( `;
-  }
-
-  alert(matchResult);
+  return WINNER;
 }
-// game(3);
 
-const numOfRounds = Number(prompt("How many round(s) do you wanna play? "));
-game(numOfRounds);
+function clickHandler() {
+  let playerChoice = this.value;
+  let computerChoice = getComputerChoice();
+  playRound(playerChoice, computerChoice);
+
+  if (!getGameStatus()) {
+    finalResultPara.textContent = endGameWinner();
+
+    // resetting all scores
+    playerScore = 0;
+    computerScore = 0;
+  }
+}
